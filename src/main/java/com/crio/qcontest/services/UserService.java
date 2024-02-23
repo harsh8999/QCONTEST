4a53282b5ef7c387c3803dc042cfc45c47e9c652
Harsh Nayak <harsh.kumar8999@gmail.com>
2024-02-23 18:38:16 +0530

    first commit

---
+++ src/main/java/com/crio/qcontest/services/UserService.java
@@ -0,0 +1,42 @@
+package com.crio.qcontest.services;
+
+ import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.crio.qcontest.constants.UserOrder;
+import com.crio.qcontest.entities.User;
+import com.crio.qcontest.repositories.IUserRepository;
+
+public class UserService{
+
+    private final IUserRepository userRepository;
+
+    public UserService(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // TODO: CRIO_TASK_MODULE_SERVICES
+    // Complete the implementation of createUser method
+    // Implementation must take care of the following cases:-
+    // 1) Create and store user in the repository.
+
+    public User createUser(String name) {
+        User user = new User(name);
+        return userRepository.save(user);
+    }
+
+    // TODO: CRIO_TASK_MODULE_SERVICES
+    // Complete the implementation of getAllUser method
+    // Implementation must take care of the following cases:-
+    // 1) Get all the users in ascending Order w.r.t score.
+    // 2) Get all the users in descending Order w.r.t score.
+
+    public List<User> getUsers(UserOrder userOrder) {
+        List<User> users = userRepository.findAll();
+        if(userOrder == UserOrder.SCORE_ASC) {
+            return users.stream().sorted((a, b) -> a.getTotalScore().compareTo(b.getTotalScore())).collect(Collectors.toList());
+        }
+        return users.stream().sorted((a, b) -> b.getTotalScore().compareTo(a.getTotalScore())).collect(Collectors.toList());
+    } 
+}
